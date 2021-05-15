f7754bfc694aff76af3d240613732a8db054076c
Grace Choi <choi.g.330@gmail.com>
2021-05-14 21:16:43 -0700

    testing

--- tests/test_creep_craze_maze.py
+++ tests/test_creep_craze_maze.py
@@ -1,14 +1,24 @@
-from creep_craze_maze.creep_craze_maze import Wall, rooms, room, thisisatest_code
+from creep_craze_maze.creep_craze_maze import *
 
+wall = Wall(400, 800, 20, 20, WHITE)
 
-# class TestPlayer(pygame.sprite.Sprite):
-
-#     @pytest.fixture
-#     def speed():
-#         image = self.image(200, 200)
-#         return image
-
-def test_thisisatest_code():
-    actual == thisisatest_code(3, 4)
-    expected == 7
+def test_rect_x():
+    actual = wall.rect.x
+    expected = 400
     assert actual == expected
+
+def test_rect_y():
+    actual = wall.rect.y
+    expected = 800
+    assert actual == expected
+
+def test_rect_width():
+    actual = wall.rect.width
+    expected = 20
+    assert actual == expected
+
+def test_rect_height():
+    actual = wall.rect.height
+    expected = 20
+    assert actual == expected
+
